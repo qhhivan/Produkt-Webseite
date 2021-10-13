f42e78a6749d9f8f4f09b692ce59f4f94f06d8c1
qhhivan <orsolic.i02@htlwienwest.at>
2021-10-13 22:03:02 +0200

    Navbar v1
    Die Links müssen schöner sein und eingebunden sein

---
+++ produkt-webseite/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+// Import Components
+import Navbar from '@/components/Navbar.vue'; 
+
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+
+  components: {
+    Navbar,
+  },
+};
+</script>
---
+++ produkt-webseite/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app-bar app color="primary" height="200">
+    <v-app-bar-title>
+      <v-img
+        src="../assets/a-team-test-logo.jpg"
+        max-height="200"
+        max-width="200"
+        contain
+      ></v-img>
+    </v-app-bar-title>
+
+    <v-breadcrumbs :items="items" divider="/"></v-breadcrumbs>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          text: 'Home',
+          disabled: false,
+          //   href: '',
+        },
+        {
+          text: 'Über unser Team',
+          disabled: false,
+          //   href: '',
+        },
+        {
+          text: 'Über das Projekt',
+          disabled: false,
+          //   href: '',
+        },
+        {
+          text: 'Next Steps',
+          disabled: false,
+          //   href: '',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
